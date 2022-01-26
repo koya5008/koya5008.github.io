4ecf74a9c0c1a03e19bb5f3adeeb32694f7ec29f
koya5008 <98443946+koya5008@users.noreply.github.com>
2022-01-26 13:50:56 +0900

    Add files via upload

---
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,29 @@
+let myImage = document.querySelector('img');
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute('src','images/firefox-icon.png');
+    }
+}
+
+function setUserName() {
+  let myName = prompt('あなたの名前を入力してください。');
+  
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Mozilla はすばらしいよ、' + myName;
+}
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla はすばらしいよ、' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
